bc5c039908aefa954ed707ec0358f2ae3a33dd66
admin <admin@gmail.com>
2024-11-04 14:26:24 +0100

    - zadanie inf 04 czerwiec 2024 desktop

---
+++ Inf04_Czerwiec_2024/desktopowa/desktopowa/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace desktopowa
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //StreamReader r = new StreamReader("Data.txt");
+
+    }
+
+}
